b5383b1ff48fc453b84bfa7f9d970b680bb3dc99
Lincoln <willakelin@seekrtech.com>
2023-02-08 23:30:21 +0100

    update: done custom unlit material

--- Assets/Custom RP/Shaders/Unlit.shader
+++ Assets/Custom RP/Shaders/Unlit.shader
@@ -1,7 +1,10 @@
 Shader "Custom RP/Unlit"
 {
     
-    Properties { }
+    Properties
+    {
+        _BaseColor ("Color", Color) = (1.0, 1.0, 1.0, 1.0)
+    }
     
     SubShader
     {
--- Assets/Custom RP/Shaders/UnlitPass.hlsl
+++ Assets/Custom RP/Shaders/UnlitPass.hlsl
@@ -3,6 +3,8 @@
     
     #include "../ShaderLibrary/Common.hlsl"
     
+    float4 _BaseColor;
+    
     float4 UnlitPassVertex(float3 positionOS: POSITION): SV_POSITION
     {
         float3 positionWS = TransformObjectToWorld(positionOS.xyz);
@@ -11,6 +13,6 @@
     
     float4 UnlitPassFragment(): SV_TARGET
     {
-        return 0.0;
+        return _BaseColor;
     }
 #endif
