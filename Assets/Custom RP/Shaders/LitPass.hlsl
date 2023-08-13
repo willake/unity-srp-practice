cdb2ea2bc61f2ea8cf43c5de8d58607940fa5099
willake <willakelin@gmail.com>
2023-08-13 10:32:42 +0800

    update: add brdf calculation and reflection

--- Assets/Custom RP/Shaders/LitPass.hlsl
+++ Assets/Custom RP/Shaders/LitPass.hlsl
@@ -4,6 +4,7 @@
     #include "../ShaderLibrary/Common.hlsl"
     #include "../ShaderLibrary/Surface.hlsl"
     #include "../ShaderLibrary/Light.hlsl"
+    #include "../ShaderLibrary/BRDF.hlsl"
     #include "../ShaderLibrary/Lighting.hlsl"
     
     TEXTURE2D(_BaseMap);
@@ -29,6 +30,7 @@
     struct Varyings
     {
         float4 positionCS: SV_POSITION;
+        float3 positionWS: VAR_POSITION;
         float3 normalWS: VAR_NORMAL;
         float2 baseUV: VAR_BASE_UV;
         UNITY_VERTEX_INPUT_INSTANCE_ID
@@ -39,8 +41,8 @@
         Varyings output;
         UNITY_SETUP_INSTANCE_ID(input);
         UNITY_TRANSFER_INSTANCE_ID(input, output);
-        float3 positionWS = TransformObjectToWorld(input.positionOS);
-        output.positionCS = TransformWorldToHClip(positionWS);
+        output.positionWS = TransformObjectToWorld(input.positionOS);
+        output.positionCS = TransformWorldToHClip(output.positionWS);
         output.normalWS = TransformObjectToWorldNormal(input.normalOS);
         float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseMap_ST);
         output.baseUV = input.baseUV * baseST.xy + baseST.zw;
@@ -65,12 +67,14 @@
 
         Surface surface;
         surface.normal = normalize(input.normalWS);
+        surface.viewDirection = normalize(_WorldSpaceCameraPos - input.positionWS);
         surface.color = base.rgb;
         surface.alpha = base.a;
         surface.metallic = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Metallic);
         surface.smoothness = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Smoothness);
 
-        float3 color = GetLighting(surface);
+        BRDF brdf = GetBRDF(surface);
+        float3 color = GetLighting(surface, brdf);
 
         return float4(color, surface.alpha);
     }
